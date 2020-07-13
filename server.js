b6bbf94c4c1c44fe4778751a9531642b431ebfff
Zachary Deacon <zackdeacon347@gmail.com>
2020-07-12 17:40:58 -0700

    changed var to const

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const DB = require("./Develop/db/db");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
